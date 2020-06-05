02011fa0d387e28e0f2d9e42c913e879c2c92646
poem <normamoras@gmail.com>
2020-06-05 10:28:38 -0400

    passing initial tests

--- newArray.js
+++ newArray.js
@@ -1,3 +1,4 @@
+
 let num = [1, 2, 3, 4, 5]
 
 //multiply the array by 2
@@ -15,3 +16,4 @@ const mapNewFn = array => array.map( el => el = el*2 )
 mapNewFn(num)
 //[2, 4, 6, 8, 10]
 
+module.exports = {mapFn, mapNewFn};
--- newArray.test.js
+++ newArray.test.js
@@ -1,8 +1,8 @@
-const newArray = require('./newArray');
+const {mapFn, mapNewFn} = require('./newArray');
 
 
 test('mapNewFn function exists', () =>{
-    expect(newArray.mapNewFn).toBeDefined();
+    expect(mapNewFn).toBeDefined();
 });
 
 test('use map to multiply array by 2', () =>{
@@ -10,5 +10,5 @@ test('use map to multiply array by 2', () =>{
    let numbers = [1, 2, 3, 4, 5];
    let result = [2, 4, 6, 8, 10]
 
-    expect(newArray.mapNewFn(numbers)).toEqual(result)
+    expect(mapNewFn(numbers)).toEqual(result)
 });
